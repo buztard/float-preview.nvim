641343214cbd79db5b844961d90c854cd8e9dbd2
Bastian Winkler <buz@netbuz.org>
2020-04-05 17:56:04 +0200

    Keep menu to show signatures

--- ncm2-plugin/float_preview.py
+++ ncm2-plugin/float_preview.py
@@ -2,9 +2,9 @@
 
 # Remove `menu` and prepend it to `info` field of items for ncm2
 
+
 def wrap():
     from ncm2_core import ncm2_core
-    from ncm2 import getLogger
     import vim
 
     if vim.vars['float_preview#docked']:
@@ -16,7 +16,6 @@ def wrap():
         matches = args[-1]
         for m in matches:
             menu = m['menu']
-            m['menu'] = ''
             info = m['info'].strip()
             if menu != info[:len(menu)]:
                 m['info'] = menu + "\n\n" + info
@@ -24,5 +23,5 @@ def wrap():
 
     ncm2_core.matches_decorate = new_matches_decorate
 
-wrap()
 
+wrap()
